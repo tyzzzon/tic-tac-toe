1c5cc1293c7b2d6e2cddc6097688d2eea5ba1441
tyzzzon <tyzzon@yandex.ru>
2018-07-08 16:00:25 +0700

    Add toggle button, highlight win combination

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
+    let winClass = 'square';
+    winClass += props.isInWinCombination ? ' win' : '';
 	return (
-		<button className="square" onClick={props.onClick}>
+		<button className={winClass} onClick={props.onClick}>
 			{props.value}
 		</button>
 	);
@@ -15,6 +17,7 @@ class Board extends React.Component {
 		return (
 			<Square 
 				value={this.props.squares[i]}
+                isInWinCombination={this.props.winArray[i]}
 				onClick={() => this.props.onClick(i)}
 			/>
 		);
@@ -50,9 +53,12 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
                 lastMove: [null, null],
+                moveNumber: 0,
             }],
             stepNumber: 0,
             xIsNext: true,
+            isReversed: false,
+            winCombination: null,
         };
     }
 
@@ -60,7 +66,8 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
+        const moveNumber = current.moveNumber;
+        if (calculateWinCombination(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X': 'O';
@@ -68,6 +75,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
                 lastMove: [i % 3 + 1, Math.floor(i / 3) + 1],
+                moveNumber: moveNumber+1,
             }]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
@@ -81,23 +89,36 @@ class Game extends React.Component {
         });
     }
 
+    changeOrder() {
+        this.setState({
+            history: this.state.history.reverse(),
+            isReversed: !this.state.isReversed,
+        });
+    }
+
 	render() {
         const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
+        const abs = Math.abs(this.state.stepNumber-history.length + 1);
+        const current = this.state.isReversed ? history[abs] : history[this.state.stepNumber];
+        const winCombination = calculateWinCombination(current.squares);
 
-        const moves = history.map((step, move) => {
-            const desc = (move ? 'Go to move #' + move : 'Go to game start') +
+        const moves = history.map((step) => {
+            const desc = (step.moveNumber ? 'Go to move #' + step.moveNumber : 'Go to game start') +
                 (step.lastMove[0] ? '(Last move: (' + step.lastMove[0] + ',' + step.lastMove[1] + '))' : '');
             return (
-                <li key={move}>
-                    <button className="history-item" onClick={() => this.jumpTo(move)}>{desc}</button>
+                <li key={step.moveNumber}>
+                    <button className="history-item" onClick={() => this.jumpTo(step.moveNumber)}>{desc}</button>
                 </li>
             );
         });
 
         let status;
-        if (winner) {
+        let winArray = Array(9).fill(null);
+        if (winCombination) {
+            for (let i = 0; i < 3; i++) {
+                winArray[winCombination[i]] = true;
+            }
+            const winner = winCombination[0];
             status = 'Winner: ' + winner;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -108,12 +129,14 @@ class Game extends React.Component {
 				<div className="game-board">
 					<Board
                         squares={current.squares}
+                        winArray={winArray}
                         onClick={(i) => this.handleClick(i)}
                     />
 				</div>
 				<div className="game-info">
 					<div>{status}</div>
 					<ol>{moves}</ol>
+                    <button className="change-order" onClick={() => this.changeOrder()}>Change Order</button>
 				</div>	
 			</div>
 		);
@@ -127,7 +150,7 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-function calculateWinner(squares) {
+function calculateWinCombination(squares) {
 	const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -141,7 +164,7 @@ function calculateWinner(squares) {
 	for (let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i];
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
+			return lines[i];
 		}
 	}
 	return null;
